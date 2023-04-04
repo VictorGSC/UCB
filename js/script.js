e7fbdc8f8e3fab3e9cfeb8f3d068dff375d9efe5
VictorGSC <vcitorgabriels.c234@gmail.com>
2023-04-03 21:09:42 -0300

    Aula de Revisão de função

---
+++ js/script.js
@@ -0,0 +1,52 @@
+//console.log("Aqui etsá um texto");
+
+/*let nome ="Jailson", idade = 33;
+
+console.log(`Seu nome é ${nome} e sua idade é ${idade} anos`);*/
+
+/*let suaIdade = prompt("Sua Idade: ")
+
+if (suaIdade >= 18) {
+    console.log("Você pode entrar!");
+} else {
+    console.log("Acesso Negado!");
+}
+
+
+function pessoa (nome, idade){
+    this.nome = nome;
+    this.idade = idade;
+    return console.log(`Olá ${nome}, sua idade é ${idade}. \n Seja bem-vindo!`);
+}
+
+let nome = prompt("DIgite Seu nome: ");
+let idade = prompt("Digite a sua idade: ");
+
+console.log(pessoa(nome, idade));
+
+
+let numero;
+
+function parImpar (numero){
+    if(numero % 2 ==0){
+        let numero = 10;
+        console.log("É par!");
+        console.log(numero);
+    } else{
+        console.log("É Ímpar!");
+    }
+}
+
+parImpar(numero);
+console.log(numero);
+*/
+
+//Function Arrow X *Const*
+
+const potencia = (numero =>)
+
+function potencia (numero){
+    return numero * numero;
+}
+
+console.log(potencia(6));
